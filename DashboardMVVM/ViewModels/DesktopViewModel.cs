dfa0412473f524da9417353fbf104eaab41c703c
potatozhb <zhaohongbo1980@gmail.com>
2022-11-10 11:31:52 -0600

    tree222 fdafasdf

--- DashboardMVVM/ViewModels/DesktopViewModel.cs
+++ DashboardMVVM/ViewModels/DesktopViewModel.cs
@@ -11,7 +11,7 @@ namespace DashboardMVVM.ViewModels
         private readonly CollectionViewSource DesktopItemsCollection;
         public ICollectionView DesktopSourceCollection => DesktopItemsCollection.View;
 
-        public DesktopViewModel() //test tree
+        public DesktopViewModel() //test tree adsfadfasdf
         {            
             ObservableCollection<DesktopItems> desktopItems = new ObservableCollection<DesktopItems>
             {
